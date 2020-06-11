93eeddaa45c52022bc51fa10cea949c9dde9823e
dave-roo <35938373+dave-roo@users.noreply.github.com>
2020-06-11 07:06:04 +0200

    working demo, issue overcome

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+struct SomethingWeWantToTest {
+    SomethingWeWantToTest() : m_value(1) {}
+    int m_value;
+};
+
+TEST_CASE("Simple example") {
+    SomethingWeWantToTest testObject;
+
+    SECTION("First section, fails") {
+        REQUIRE(testObject.m_value == 0);
+    }
+    SECTION("Second section, works") {
+        REQUIRE(testObject.m_value == 1);
+    }
+}
